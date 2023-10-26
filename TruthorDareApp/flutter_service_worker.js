fcde5e103b0d884b4f4104c88a43d8f28a50cb79
Abhishek Survase <abhishek.survase2000@gmail.com>
2023-10-27 01:21:43 +0530

    Fixed overlapping background on qs screen

--- TruthorDareApp/flutter_service_worker.js
+++ TruthorDareApp/flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "4434b56ba68ea53f268420986dee4bc0
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f02186b6fd8e9899833da25014bf956c",
-"/": "f02186b6fd8e9899833da25014bf956c",
-"main.dart.js": "1b9dd0d2fca59cdf4e81dc9265528207",
+"index.html": "aabf0e4b3836bf44d1065411421073f5",
+"/": "aabf0e4b3836bf44d1065411421073f5",
+"main.dart.js": "3957dada7570a29f433204f83c14a69d",
 "manifest.json": "942d6c9521b8d8ad255d9a05ac6fc620",
 "version.json": "7f1b4cef170b22435195f44954b67b23"};
 // The application shell files that are downloaded before a service worker can
